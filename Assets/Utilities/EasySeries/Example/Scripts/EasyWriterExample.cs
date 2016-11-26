9782bcd4158376a99d088809626e7587c23931d4
chaolun <0chaolun0@gmail.com>
2016-11-26 17:49:14 +0800

    add easywriter.cs try to code a script that can help dev easy to save any data.

---
+++ Assets/Utilities/EasySeries/Example/Scripts/EasyWriterExample.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+// create by chaolun 2016/11/26
+public class EasyWriterExample : MonoBehaviour
+{
+
+	void Start ()
+	{
+		Hashtable hs = new Hashtable ();
+		hs.Add ("byte", 0x01010101);
+		hs.Add ("int", 9876543210);
+		hs.Add ("float", 0.123456789f);
+		EasyWriter.Serialize (Application.persistentDataPath + "/example.json", hs);
+//		string path = Path.GetFullPath (Application.persistentDataPath);
+//		System.Diagnostics.Process.Start ("explorer.exe", path);
+
+		Hashtable hashtable = EasyWriter.Deserialize (Application.persistentDataPath + "/example.json");
+		IDictionaryEnumerator de = hashtable.GetEnumerator ();
+		while (de.MoveNext ()) {
+			Debug.Log (de.Key + " : " + de.Value);
+		}
+	}
+}
---
+++ Assets/Utilities/EasySeries/Scripts/EasyWriter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+// create by chaolun 2016/11/26
+public class EasyWriter
+{
+
+	static public void Serialize (string path, Hashtable hashtable)
+	{
+		Hashtable hs = Deserialize (path);
+		if (hashtable != null && hs != null) {
+			IEnumerator enumerator = hs.Keys.GetEnumerator ();
+			while (enumerator.MoveNext ()) {
+				if (!hashtable.ContainsKey (enumerator.Current)) {
+					hashtable.Add (enumerator.Current, hs [enumerator.Current]);
+				}
+			}
+		}
+		FileStream fs = new FileStream (path, FileMode.OpenOrCreate);
+		BinaryFormatter formatter = new BinaryFormatter ();
+		try {
+			formatter.Serialize (fs, hashtable);
+		} catch (SerializationException e) {
+			Debug.LogError ("Failed to serialize. Reason: " + e.Message);
+			throw;
+		} finally {
+			fs.Close ();
+		}
+	}
+
+	static public Hashtable Deserialize (string path)
+	{
+		Hashtable hashtable = null;
+		if (File.Exists (path)) {
+			FileStream fs = new FileStream (path, FileMode.Open);
+			try {
+				BinaryFormatter formatter = new BinaryFormatter ();
+				hashtable = (Hashtable)formatter.Deserialize (fs);
+			} catch (SerializationException e) {
+				Debug.LogError ("Failed to deserialize. Reason: " + e.Message);
+				throw;
+			} finally {
+				fs.Close ();
+			}
+		}
+		return hashtable;
+	}
+}

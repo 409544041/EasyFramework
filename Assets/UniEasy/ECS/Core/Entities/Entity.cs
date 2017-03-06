16c85b34d2aa0dd5e60e555dfb39457488a58da8
chaolun <0chaolun0@gmail.com>
2017-03-06 21:40:23 +0800

    add entity logic

---
+++ Assets/UniEasy/ECS/Core/Entities/Entity.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace UniEasy.ECS
+{
+	public class Entity : IEntity
+	{
+		private readonly Dictionary<Type, object> components;
+
+		public int Id { get; private set; }
+
+		public IEnumerable<object> Components { get { return components.Values; } }
+
+		public Entity (int id, IEventSystem eventSystem)
+		{
+			Id = id;
+			EventSystem = eventSystem;
+			components = new Dictionary<Type, object> ();
+		}
+
+		public void AddComponent (object component)
+		{
+			components.Add (component.GetType (), component);
+		}
+
+		public void AddComponent<T> () where T : class, new()
+		{
+			AddComponent (new T ());
+		}
+
+		public void RemoveComponent (object component)
+		{
+			if (!components.ContainsKey (component.GetType ())) {
+				return;
+			}
+
+			if (component is IDisposable) {
+				(component as IDisposable).Dispose ();
+			}
+
+			components.Remove (component.GetType ());
+		}
+
+		public void RemoveComponent<T> () where T : class
+		{
+			if (!HasComponent<T> ()) {
+				return;
+			}
+
+			var component = GetComponent<T> ();
+			RemoveComponent (component);
+		}
+
+		public bool HasComponent<T> () where T : class
+		{
+			return components.ContainsKey (typeof(T));
+		}
+
+		public bool HasComponents (params Type[] componentTypes)
+		{
+			if (components.Count == 0) {
+				return false;
+			}
+
+			return componentTypes.All (x => components.ContainsKey (x));
+		}
+
+		public T GetComponent<T> () where T : class
+		{
+			return components [typeof(T)] as T;
+		}
+	}
+}
---
+++ Assets/UniEasy/ECS/Core/Entities/IEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace UniEasy.ECS
+{
+	public interface IEntity
+	{
+		int Id { get; }
+
+		IEnumerable<object> Components { get; }
+
+		void AddComponent (object component);
+
+		void AddComponent<T> () where T : class, new();
+
+		void RemoveComponent (object component);
+
+		void RemoveComponent<T> () where T : class;
+
+		T GetComponent<T> () where T : class;
+
+		bool HasComponent<T> () where T : class;
+
+		bool HasComponents (params Type[] component);
+	}
+}
